90cf20bd699d2800159ae22b3071c2f14aa48b20
chellesalucop <mrasalucop01@tip.edu.ph>
2026-04-09 11:35:14 +0800

    Add SSL option for Gmail SMTP alternative port

--- nail_booking/settings.py
+++ nail_booking/settings.py
@@ -231,6 +231,7 @@ USE_TZ = True
 EMAIL_HOST = env('EMAIL_HOST', default='smtp.gmail.com')
 EMAIL_PORT = env.int('EMAIL_PORT', default=587)
 EMAIL_USE_TLS = env.bool('EMAIL_USE_TLS', default=True)
+EMAIL_USE_SSL = env.bool('EMAIL_USE_SSL', default=False)
 EMAIL_HOST_USER = env('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
 DEFAULT_FROM_EMAIL = env('DEFAULT_FROM_EMAIL', default='noreply@polishpalette.com')
