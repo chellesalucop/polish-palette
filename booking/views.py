12d3473c97ca1f6cba81b47f5e3e6f1f34f475b9
chellesalucop <mrasalucop01@tip.edu.ph>
2026-04-10 15:51:46 +0800

    Add missing get_object_or_404 import in views.py

--- booking/views.py
+++ booking/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.contrib.auth import authenticate, login, logout, get_user_model, update_session_auth_hash
 from django.contrib.auth.decorators import login_required
